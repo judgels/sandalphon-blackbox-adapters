733bd724456ee380bfb4da4795fa3913b461e07e
Ashar Fuadi <fushar@gmail.com>
2015-02-23 21:22:11 +0700

    Add auto population feature for batch grading engine

--- app/org/iatoki/judgels/sandalphon/programming/adapters/BatchGradingConfigAdapter.java
+++ app/org/iatoki/judgels/sandalphon/programming/adapters/BatchGradingConfigAdapter.java
@@ -1,6 +1,10 @@
 package org.iatoki.judgels.sandalphon.programming.adapters;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import org.apache.commons.io.FilenameUtils;
 import org.iatoki.judgels.gabriel.GradingConfig;
+import org.iatoki.judgels.gabriel.blackbox.TestCase;
 import org.iatoki.judgels.gabriel.blackbox.TestGroup;
 import org.iatoki.judgels.gabriel.blackbox.configs.BatchGradingConfig;
 import org.iatoki.judgels.sandalphon.programming.Problem;
@@ -12,7 +16,7 @@ import play.twirl.api.Html;
 import java.io.File;
 import java.util.List;
 
-public final class BatchGradingConfigAdapter extends SingleSourceFileWithoutSubtasksBlackBoxGradingConfigAdapter {
+public final class BatchGradingConfigAdapter extends SingleSourceFileWithoutSubtasksBlackBoxGradingConfigAdapter implements ConfigurableWithAutoPopulation {
     @Override
     public Form<?> createFormFromConfig(GradingConfig config) {
         BatchGradingConfigForm form = new BatchGradingConfigForm();
@@ -58,10 +62,39 @@ public final class BatchGradingConfigAdapter extends SingleSourceFileWithoutSubt
     }
 
     @Override
+    public GradingConfig updateConfigWithAutoPopulation(GradingConfig config, List<File> testDataFiles) {
+        ImmutableList.Builder<TestCase> testCases = ImmutableList.builder();
+
+        for (int i = 0; i + 1 < testDataFiles.size(); i++) {
+            String in = testDataFiles.get(i).getName();
+            String out = testDataFiles.get(i + 1).getName();
+            if (isTestCasePair(in, out)) {
+                testCases.add(new TestCase(in, out, ImmutableSet.of(-1)));
+                i++;
+            }
+        }
+
+        List<TestGroup> testData = ImmutableList.of(new TestGroup(0, ImmutableList.of()), new TestGroup(-1, testCases.build()));
+
+        BatchGradingConfig castConfig = (BatchGradingConfig) config;
+        return new BatchGradingConfig(castConfig.getTimeLimitInMilliseconds(), castConfig.getMemoryLimitInKilobytes(), testData, castConfig.getCustomScorer());
+    }
+
+    @Override
     public Html renderUpdateGradingConfig(Form<?> form, Problem problem, List<File> testDataFiles, List<File> helperFiles) {
         @SuppressWarnings("unchecked")
         Form<BatchGradingConfigForm> castForm = (Form<BatchGradingConfigForm>) form;
 
         return batchGradingConfigView.render(castForm, problem, testDataFiles, helperFiles);
+    }
+
+    private boolean isTestCasePair(String in, String out) {
+        String inBaseName = FilenameUtils.getBaseName(in);
+        String inExtension = FilenameUtils.getExtension(in);
+
+        String outBaseName = FilenameUtils.getBaseName(out);
+        String outExtension = FilenameUtils.getExtension(out);
+
+        return inBaseName.equals(outBaseName) && inExtension.equals("in") && outExtension.equals("out");
     }
 }
